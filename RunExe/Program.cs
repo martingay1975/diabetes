bd84481395a13dce7fd8d63330c232c7756c25f1
Martin Gay <mgay@highlight.net>
2022-05-16 08:06:39 +0100

    Updates

--- RunExe/Program.cs
+++ RunExe/Program.cs
@@ -2,7 +2,7 @@ using Helper.Nightscout;
 using Helper;
 using RunExe;
 
-const string diasendCsvPath = @"c:\git\diabetes\25AprTo8May.csv";
+const string diasendCsvPath = @"c:\git\diabetes\08MayTo15May.csv";
 var settings = await SettingsModel.LoadAsync();
 var diasendToNightscoutTreatmentsManager = new DiasendToNightscoutTreatmentsManager(() => new NightscoutClient(settings.ApiSecretSha1Hash, settings.Host, settings.AllowNightscoutWrite));
 settings.LastTreatmentProcessDateTime = await diasendToNightscoutTreatmentsManager.TransferAsync(diasendCsvPath, settings.LastTreatmentProcessDateTime);
