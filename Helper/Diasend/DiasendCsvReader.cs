354e06fb0f41b7e5c5ab980d2cf182ca4db4b1fa
Martin Gay <mgay@highlight.net>
2024-01-23 11:59:37 +0000

    Move from Diasend to Glokoo

--- Helper/Diasend/DiasendCsvReader.cs
+++ Helper/Diasend/DiasendCsvReader.cs
@@ -1,47 +1,55 @@
-using CsvHelper;
 using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace Helper.Diasend
 {
-    public class DiasendCsvReader
-    {
-        public IEnumerable<InsulinAdministration> Read(string csvpath)
-        {
-            var output = new Dictionary<string, InsulinAdministration>();
+	public class DiasendCsvReader
+	{
+		public IEnumerable<InsulinAdministration> Read(string csvpath)
+		{
+			var output = new Dictionary<string, InsulinAdministration>();
 
-            using (var reader = new StreamReader(csvpath))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                var records = csv.GetRecords<InsulinAdministration>().ToList();
+			var csvReaderConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+			{
+				HasHeaderRecord = false,
+			};
 
-                foreach (var record in records)
-                {
-                    Console.WriteLine($"Time:{record.Time}  Basal:{record.BasalAmount}   Bolus:{record.BolusVolume}     Carbs:{record.Carbs}");
-                    if (output.TryGetValue(record.Time, out var insulinAdministration))
-                    {
+			using (var reader = new StreamReader(csvpath))
+			using (var csv = new CsvReader(reader, csvReaderConfig))
+			{
+
+				var records = csv.GetRecords<InsulinAdministration>().ToList();
+
+				foreach (var record in records)
+				{
+					Console.WriteLine($"Time:{record.Time}  Basal:{record.BasalAmount}   Bolus:{record.BolusVolume}     Carbs:{record.Carbs}");
+					if (output.TryGetValue(record.Time, out var insulinAdministration))
+					{
 						Console.WriteLine($"Update existing Time:{record.Time}");
 						if (!string.IsNullOrWhiteSpace(record.BasalAmount))
-                        {
-                            insulinAdministration.BasalAmount = record.BasalAmount;
-                        }
+						{
+							insulinAdministration.BasalAmount = record.BasalAmount;
+						}
 
-                        if (!string.IsNullOrWhiteSpace(record.BolusVolume))
-                        {
-                            insulinAdministration.BolusVolume = record.BolusVolume;
-                        }
+						if (!string.IsNullOrWhiteSpace(record.BolusVolume))
+						{
+							insulinAdministration.BolusVolume = record.BolusVolume;
+						}
 
-                        if (!string.IsNullOrWhiteSpace(record.Carbs))
-                        {
-                            insulinAdministration.Carbs = record.Carbs;
-                        }
-                    } else
-                    {
-                        output.Add(record.Time, record);
-                    }
-                }
+						if (!string.IsNullOrWhiteSpace(record.Carbs))
+						{
+							insulinAdministration.Carbs = record.Carbs;
+						}
+					}
+					else
+					{
+						output.Add(record.Time, record);
+					}
+				}
 
 				return output.Values;
 			}
-        }
-    }
+		}
+	}
 }
